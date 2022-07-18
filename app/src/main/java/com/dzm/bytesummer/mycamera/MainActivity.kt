d2468abbd556d8ee1f14297da37392ec635da93c
ymgwjk <1658545871@qq.com>
2022-07-18 16:24:55 +0800

    New project.

--- app/src/main/java/com/dzm/bytesummer/mycamera/MainActivity.kt
+++ app/src/main/java/com/dzm/bytesummer/mycamera/MainActivity.kt
@@ -2,10 +2,14 @@ package com.dzm.bytesummer.mycamera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dzm.bytesummer.mycamera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
+
 }
